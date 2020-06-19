54123880a1fb3017f61b979a58a232c09c22704a
Andrew Stacey <koalasthegreat@gmail.com>
2020-06-19 13:16:56 -0230

    added player checks to commands

--- src/main/java/dev/koalas/craftmail/CommandCheckMail.kt
+++ src/main/java/dev/koalas/craftmail/CommandCheckMail.kt
@@ -2,10 +2,14 @@ package dev.koalas.craftmail
 
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 class CommandCheckMail(plugin: CraftMail?) : CommandBase<CraftMail?>(plugin) {
     override fun runCommand(sender: CommandSender?, command: Command?, label: String?, args: Array<String>?): Boolean {
-        sender!!.sendMessage("Subcommand checkmail executed")
-        return true
+        if (sender!! is Player) {
+            sender.sendMessage("Subcommand checkmail executed")
+            return true
+        }
+        else return false
     }
 }
--- src/main/java/dev/koalas/craftmail/CommandSendMail.kt
+++ src/main/java/dev/koalas/craftmail/CommandSendMail.kt
@@ -2,10 +2,14 @@ package dev.koalas.craftmail
 
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 class CommandSendMail(plugin: CraftMail?) : CommandBase<CraftMail?>(plugin) {
     override fun runCommand(sender: CommandSender?, command: Command?, label: String?, args: Array<String>?): Boolean {
-        sender!!.sendMessage("Subcommand sendmail executed")
-        return true
+        if (sender!! is Player) {
+            sender.sendMessage("Subcommand sendmail executed")
+            return true
+        }
+        else return false
     }
 }
